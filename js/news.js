6da7dbf81c5183d60c3eb85c20ccca98a375366e
seergi93 <sergiroberti93@gmail.com>
2017-05-15 18:22:59 +0200

    intentat utilitzar json

--- js/news.js
+++ js/news.js
@@ -1,21 +1,31 @@
 //carregar noticies QuePodemFer
 
-/*
+
+
 $(function() {
-    $.getJSON("data/QuePodemFer.json", function(jsonObject) {
-        posarNoticiesQuePodemFer(jsonObject);
-    })
+    //cargar comunidades
+    $.getJSON("data/comunidades.json", function(jsonObject) {
+        ponerComunidades(jsonObject);
+    });
+
+    //onclick en verinfo
+    $("#omplirJS").click(function() {
+        var v = $("#comunidades").val();
+        if (v != 0) window.location = "/comunidad/" + $("#comunidades").val();
+        else alert('Selecciona una comunidad');
+    });
 });
-/*
 
-document.getElementById("OmplirJS").innerHTML = "Hello JavaScript!";
+function ponerComunidades(json) {
+    $.each(json, function(i, comunidad) {
+        //dos formas de hacer lo mismo, la segunda es más adecuada.
+        //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
+        $('#comunidades').append($('<option>', { value: comunidad.slug, text: comunidad.comunidad }));
+        //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
+    });
+}
 
-/*
-posarNoticiesQuePodemFer() {
 
-document.write.toString("asdiofhsagsh");
-
-};*/
 
 $(document).ready(function() {
     var count = 0;
@@ -28,7 +38,8 @@ $(document).ready(function() {
         if ($(document).scrollTop() + $(window).height() >= $(document).height()) {
 
 
-            document.getElementById("omplirJS").innerHTML = "PROVAM QUE VA BE SCROLL";
+
+            document.getElementById("omplirJS").innerHTML = ;
 
 
 
