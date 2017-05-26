8f9c1318fa792eabcd90136284d25ad23dc39429
seergi93 <sergiroberti93@gmail.com>
2017-05-26 17:01:33 +0200

    js

--- js/news.js
+++ js/news.js
@@ -29,21 +29,21 @@ $(function() {
 });
 
 function ponerNoticias(json) {
-    $.each(json, function(j, item) {
+    $.each(json, function(j, objecte) {
 
 
         $('.insertarJson').append('<div class="card">' +
-            item.imatge +
+            objecte.imatge +
             '<div class="card-block">' +
-            '<h2 class="card-title">' + item.titol + '.</h2>' +
-            '<p class="card-text">' + item.descripcio +
+            '<h2 class="card-title">' + objecte.titol + '.</h2>' +
+            '<p class="card-text">' + objecte.descripcio +
             '</p>' +
             '<div class="row justify-content-around">' +
             '<button class="btn btn-success col col-lg-2 col-md-4 col-xs-2" type="button" data-toggle="collapse" data-target="#' + item.id + '"' +
             'aria-expanded="false" aria-controls="collapseExample"> Llegir més </button>' +
             '<div class="fb-share-button btn btn-primary col col-lg-2 col-md-4 col-xs-2" data-href="http://sergiroberti.com/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore " id="lletraBlanca" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fsergiroberti.com%2F&amp;src=sdkpreparse">Comparteix</a></div>' +
-            '<div class="collapse" id="' + item.id + '">' +
-            '<div class="card-text"><p></p>' + item.descripcio2 +
+            '<div class="collapse" id="' + objecte.id + '">' +
+            '<div class="card-text"><p></p>' + objecte.descripcio2 +
             ' </div> </div>  </p>  </div></div></div><p></p>');
 
     });
